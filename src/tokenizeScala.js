a8b07614f7a2fa049e96c8da4cf777b971337a6f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-03 19:40:28 +0100

    fix: highlighting of numbers (#3)

--- src/tokenizeScala.js
+++ src/tokenizeScala.js
@@ -129,6 +129,9 @@ export const tokenizeLine = (line, lineState) => {
         } else if ((next = part.match(RE_LINE_COMMENT))) {
           token = TokenType.Comment
           state = State.TopLevelContent
+        } else if ((next = part.match(RE_NUMERIC))) {
+          token = TokenType.Numeric
+          state = State.TopLevelContent
         } else if ((next = part.match(RE_ANYTHING))) {
           token = TokenType.Text
           state = State.TopLevelContent
