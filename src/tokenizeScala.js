446de41b72d88724de9fd33b9bf31ac1f4d42f8a
Le Vivilet <levivlet@pm.me>
2023-01-03 15:36:44 +0100

    fix: add highlighting for scala keywords

--- src/tokenizeScala.js
+++ src/tokenizeScala.js
@@ -1,13 +1,64 @@
 /**
  * @enum number
  */
+export const State = {
+  TopLevelContent: 1,
+  InsideDoubleQuoteString: 2,
+}
+
+export const StateMap = {
+  [State.TopLevelContent]: 'TopLevelContent',
+}
+
+/**
+ * @enum number
+ */
 export const TokenType = {
-  Text: 1,
+  None: 1,
+  Whitespace: 2,
+  PunctuationString: 3,
+  String: 4,
+  Keyword: 5,
+  Numeric: 6,
+  Punctuation: 7,
+  VariableName: 8,
+  Comment: 885,
+  Text: 9,
+  KeywordControl: 10,
+  Function: 11,
+  LanguageConstant: 12,
+  KeywordReturn: 13,
 }
 
 export const TokenMap = {
+  [TokenType.None]: 'None',
+  [TokenType.Whitespace]: 'Whitespace',
+  [TokenType.PunctuationString]: 'PunctuationString',
+  [TokenType.String]: 'String',
+  [TokenType.Keyword]: 'Keyword',
+  [TokenType.Numeric]: 'Numeric',
+  [TokenType.Punctuation]: 'Punctuation',
+  [TokenType.VariableName]: 'VariableName',
+  [TokenType.Comment]: 'Comment',
   [TokenType.Text]: 'Text',
+  [TokenType.KeywordControl]: 'KeywordControl',
+  [TokenType.Function]: 'Function',
+  [TokenType.LanguageConstant]: 'LanguageConstant',
+  [TokenType.KeywordReturn]: 'KeywordReturn',
 }
+
+const RE_LINE_COMMENT = /^\/\/.*/s
+const RE_WHITESPACE = /^\s+/
+const RE_KEYWORD =
+  /^(?:abstract|case|catch|def|do|else|extends|false|final|finally|for|forSome|if|implicit|import|lazy|match|new|null|object|override|package|private|protected|return|sealed|super|this|throw|trait|true|try|type|val|var|while|with|yield)\b/
+const RE_VARIABLE_NAME = /^[a-zA-Z\_\/\-\$][a-zA-Z\_\/\-\$#\d\-]*/
+const RE_PUNCTUATION = /^[:,;\{\}\[\]\.=\(\)<>\!\|\+\&\>\)]/
+const RE_NUMERIC = /^\d+(?=\s|$)/
+const RE_STRING_ESCAPE = /^\\./
+const RE_BACKSLASH_AT_END = /^\\$/
+const RE_ANYTHING = /^.+/s
+const RE_QUOTE_DOUBLE = /^"/
+const RE_STRING_DOUBLE_QUOTE_CONTENT = /^[^"\\]+/
 
 export const initialLineState = {
   state: 1,
@@ -18,11 +69,97 @@ export const hasArrayReturn = true
 
 /**
  * @param {string} line
+ * @param {any} lineState
  */
-export const tokenizeLine = (line) => {
-  const tokens = [TokenType.Text, line.length]
+export const tokenizeLine = (line, lineState) => {
+  let next = null
+  let index = 0
+  let tokens = []
+  let token = TokenType.None
+  let state = lineState.state
+  let stringEnd = lineState.stringEnd
+  const knownFunctionNames = new Set(lineState.knownFunctionNames)
+  while (index < line.length) {
+    const part = line.slice(index)
+    switch (state) {
+      case State.TopLevelContent:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_KEYWORD))) {
+          state = State.TopLevelContent
+          switch (next[0]) {
+            case 'case':
+            case 'do':
+            case 'else':
+            case 'for':
+            case 'forSome':
+            case 'if':
+            case 'while':
+              token = TokenType.KeywordControl
+              break
+            case 'true':
+            case 'false':
+              token = TokenType.LanguageConstant
+              break
+            case 'return':
+              token = TokenType.KeywordReturn
+              break
+            default:
+              token = TokenType.Keyword
+              break
+          }
+        } else if ((next = part.match(RE_PUNCTUATION))) {
+          token = TokenType.Punctuation
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_VARIABLE_NAME))) {
+          token = TokenType.VariableName
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_NUMERIC))) {
+          token = TokenType.Numeric
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_QUOTE_DOUBLE))) {
+          token = TokenType.PunctuationString
+          state = State.InsideDoubleQuoteString
+        } else if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_ANYTHING))) {
+          token = TokenType.Text
+          state = State.TopLevelContent
+        } else {
+          part //?
+          throw new Error('no')
+        }
+        break
+      case State.InsideDoubleQuoteString:
+        if ((next = part.match(RE_QUOTE_DOUBLE))) {
+          token = TokenType.PunctuationString
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_STRING_DOUBLE_QUOTE_CONTENT))) {
+          token = TokenType.String
+          state = State.InsideDoubleQuoteString
+        } else if ((next = part.match(RE_STRING_ESCAPE))) {
+          token = TokenType.String
+          state = State.InsideDoubleQuoteString
+        } else if ((next = part.match(RE_BACKSLASH_AT_END))) {
+          token = TokenType.String
+          state = State.InsideDoubleQuoteString
+        } else {
+          throw new Error('no')
+        }
+        break
+      default:
+        throw new Error('no')
+    }
+    const tokenLength = next[0].length
+    index += tokenLength
+    tokens.push(token, tokenLength)
+  }
   return {
-    state: 1,
+    state,
     tokens,
+    knownFunctionNames,
+    stringEnd,
   }
 }
